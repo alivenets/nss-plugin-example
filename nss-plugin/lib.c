422fef83b7e677982e6c6edce002314d684f25cc
Alexander Livenets <a.livenets@gmail.com>
2020-11-12 23:18:35 +0100

    nss-plugin: Use UserDB via P2P D-Bus connection
    Signed-off-by: Alexander Livenets <a.livenets@gmail.com>

--- nss-plugin/lib.c
+++ nss-plugin/lib.c
@@ -1,19 +1,22 @@
 #include <nss.h>
 
-#include <pwd.h>
 #include <grp.h>
+#include <pwd.h>
 
 #include <assert.h>
 #include <errno.h>
 #include <limits.h>
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include <client.h>
+
 #include "helpers.h"
 
-typedef struct GetentData {
+typedef struct GetentData
+{
     /*
      * Comment from nss-systemd:
      * As explained in NOTES section of getpwent_r(3) as 'getpwent_r() is not really reentrant since it
@@ -23,157 +26,160 @@ typedef struct GetentData {
      */
     pthread_mutex_t mutex;
     size_t array_index;
-    size_t count;
+    char **ext_db_entities;
+    size_t entity_count;
 } GetentData;
 
-static GetentData getgrent_data = {
-    .mutex = PTHREAD_MUTEX_INITIALIZER
-};
+static GetentData getgrent_data = {.mutex = PTHREAD_MUTEX_INITIALIZER};
 
-/*
- * The NSS plugin will return list of users with groups from the static configuration
- */
-static const char *dynamic_group_membership[]= { "com_example_dynamicuser", NULL };
-
-static const struct group group_array[] = {
-    {
-        .gr_name = (char*) "com_example_dynamicuser",
-        .gr_gid = 100000,
-        .gr_passwd = (char*) "*", /* locked */
-        .gr_mem = (char*[]) { NULL },
-    },
-    {
-        .gr_name = (char*) "service-client",
-        .gr_gid = 1001,
-        .gr_passwd = (char*) "x", /* locked */
-        .gr_mem = (char*[]) { NULL },
-    },
-};
-
-static const struct passwd user_array[] = {
-    {
-        .pw_uid = 100000,
-        .pw_gid = 100000,
-        .pw_name = "com_example_dynamicuser",
-        .pw_passwd = "x", /* locked */
-        .pw_gecos = (char*) "com.example.dynamicuser",
-        .pw_dir = (char*) "/nonexistent",
-        .pw_shell = (char*) "/bin/false",
-    },
-};
-
-static bool file_exists(char *filename)
+static char *init_group_struct(struct group *grp, char *buffer, size_t buflen, size_t *outlen)
 {
-    FILE *file = NULL;
+    char *bufPos = buffer;
+    int count = 0;
 
-    if ((file = fopen(filename, "r")) == NULL) {
-        return false;
-    } else {
-      fclose(file);
-    }
-    return true;
+    memset(grp, 0, sizeof(*grp));
+
+    count = sprintf(bufPos, "%s", "*");
+    grp->gr_passwd = bufPos;
+    bufPos += count + 1;
+
+    if (outlen)
+        *outlen = count;
+
+    return bufPos;
 }
 
-static int copy_to_passwd(const struct passwd *user_info, struct passwd *result, char *buffer, size_t buf_len)
+static char *init_passwd_struct(struct passwd *pwd, char *buffer, size_t buflen, size_t *outlen)
 {
-    *result = *user_info;
-    return 0;
+    char *bufPos = buffer;
+    int count = 0;
+
+    memset(pwd, 0, sizeof(*pwd));
+
+    count = sprintf(bufPos, "%s", "x");
+    pwd->pw_passwd = bufPos;
+    bufPos += count + 1;
+
+    count = sprintf(bufPos, "%s", "/nonexistent");
+    pwd->pw_dir = bufPos;
+    bufPos += count + 1;
+
+    count = sprintf(bufPos, "%s", "/bin/false");
+    pwd->pw_shell = bufPos;
+    bufPos += count + 1;
+
+    if (outlen)
+        *outlen = count;
+
+    return bufPos;
 }
 
-static int copy_to_group(const struct group *group_info, struct group *result, char *buffer, size_t buf_len)
+enum nss_status _nss_example_getpwnam_r(
+        const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
-    *result = *group_info;
+    char *bufPos = init_passwd_struct(result, buffer, buflen, NULL);
+    UserEntry userEntry;
 
-    if (file_exists("/tmp/enable-dynamic-group") && strcmp(result->gr_name, "service-client") == 0) {
-        result->gr_mem = (char**)dynamic_group_membership;
+    int ret = get_user_by_name(name, &userEntry);
+
+    if (ret < 0) {
+        return NSS_STATUS_NOTFOUND;
     }
 
-    return 0;
-}
+    result->pw_uid = userEntry.uid;
+    result->pw_gid = userEntry.gid;
 
-enum nss_status _nss_example_getpwnam_r(const char *name, struct passwd *result, char *buffer, size_t buflen, int *errnop)
-{
-    memset(result, 0, sizeof(*result));
+    int count = sprintf(bufPos, "%s", userEntry.name);
+    result->pw_name = bufPos;
+    bufPos += count + 1;
 
-    for (size_t i = 0; i < countof(user_array); ++i) {
-        const struct passwd *user_info = &user_array[i];
-        if (strcmp(name, user_info->pw_name) == 0) {
-            int ret = copy_to_passwd(user_info, result, buffer, buflen);
+    free_user_entry(&userEntry);
 
-            if (ret == 0)
-                return NSS_STATUS_SUCCESS;
-            else {
-                if (errnop && ret != 0)
-                    *errnop = ret;
-                return NSS_STATUS_TRYAGAIN;
-            }
-        }
-    }
-
-    return NSS_STATUS_NOTFOUND;
+    return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_example_getpwuid_r(uid_t uid, struct passwd *result, char *buffer, size_t buflen, int *errnop)
 {
-    for (size_t i = 0; i < countof(user_array); ++i) {
-        const struct passwd *user_info = &user_array[i];
+    char *bufPos = init_passwd_struct(result, buffer, buflen, NULL);
+    UserEntry userEntry;
+    int ret = get_user_by_id(uid, &userEntry);
 
-        if (uid == user_info->pw_uid) {
-            if (errnop)
-                *errnop = copy_to_passwd(user_info, result, buffer, buflen);
-            return NSS_STATUS_SUCCESS;
-        }
+    if (ret < 0) {
+        return NSS_STATUS_NOTFOUND;
     }
 
-    return NSS_STATUS_NOTFOUND;
+    result->pw_uid = userEntry.uid;
+    result->pw_gid = userEntry.gid;
+
+    int count = sprintf(bufPos, "%s", userEntry.name);
+    result->pw_name = bufPos;
+    bufPos += count + 1;
+
+    free_user_entry(&userEntry);
+
+    return NSS_STATUS_SUCCESS;
 }
 
-enum nss_status _nss_example_getgrnam_r(const char *name, struct group *result, char *buffer, size_t buflen, int *errnop)
+void copy_group_entry(const GroupEntry *entry, struct group *result, char *buffer, size_t buflen)
 {
-    memset(result, 0, sizeof(*result));
+    char *bufPos = buffer;
+    int count = 0;
 
-    for (size_t i = 0; i < countof(group_array); ++i) {
-        const struct group *group_info = &group_array[i];
-        if (strcmp(name, group_info->gr_name) == 0) {
-            int ret = copy_to_group(group_info, result, buffer, buflen);
-            if (ret == 0)
-                return NSS_STATUS_SUCCESS;
-            else {
-                if (errnop && ret != 0)
-                    *errnop = ret;
-                return NSS_STATUS_TRYAGAIN;
-            }
+    count = sprintf(bufPos, "%s", entry->name);
+    result->gr_name = bufPos;
+    bufPos += count + 1;
+
+    result->gr_gid = entry->gid;
+
+    result->gr_mem = (char **)bufPos;
+    memset(result->gr_mem, 0, sizeof(char *) * (count + 1));
+
+    bufPos += sizeof(char *) * (count + 1);
+    if (entry->membersCount > 0) {
+        for (size_t i = 0; i < entry->membersCount; i++) {
+            count = sprintf(bufPos, "%s", entry->members[i]);
+            result->gr_mem[i] = bufPos;
+            bufPos += count + 1;
         }
     }
+}
 
-    return NSS_STATUS_NOTFOUND;
+enum nss_status _nss_example_getgrnam_r(
+        const char *name, struct group *result, char *buffer, size_t buflen, int *errnop)
+{
+    char *bufPos = init_group_struct(result, buffer, buflen, NULL);
+    GroupEntry entry;
+
+    int ret = get_group_by_name(name, &entry);
+
+    if (ret < 0)
+        return NSS_STATUS_NOTFOUND;
+
+    copy_group_entry(&entry, result, bufPos, buflen);
+    free_group_entry(&entry);
+    return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_example_getgrgid_r(gid_t gid, struct group *result, char *buffer, size_t buflen, int *errnop)
 {
-    for (size_t i = 0; i < countof(group_array); ++i) {
-        const struct group *group_info = &group_array[i];
-        if (gid == group_info->gr_gid) {
-            if (errnop)
-                *errnop = copy_to_group(group_info, result, buffer, buflen);
-            return NSS_STATUS_SUCCESS;
-        }
-    }
+    char *bufPos = init_group_struct(result, buffer, buflen, NULL);
+    GroupEntry entry;
+    int ret = get_group_by_id(gid, &entry);
 
-    return NSS_STATUS_NOTFOUND;
+    if (ret < 0)
+        return NSS_STATUS_NOTFOUND;
+
+    copy_group_entry(&entry, result, bufPos, buflen);
+    free_group_entry(&entry);
+    return NSS_STATUS_SUCCESS;
 }
 
 enum nss_status _nss_example_endgrent(void)
 {
-    GetentData *p = &getgrent_data;
-
-    assert(p);
-
     __attribute__((cleanup(pthread_mutex_unlock_assertp))) pthread_mutex_t *_l = NULL;
-    _l = pthread_mutex_lock_assert(&p->mutex);
+    _l = pthread_mutex_lock_assert(&getgrent_data.mutex);
 
-    p->array_index = (size_t)-1;
-    p->count = 0;
+    getgrent_data.array_index = (size_t)-1;
 
     return NSS_STATUS_SUCCESS;
 }
@@ -181,16 +187,17 @@ enum nss_status _nss_example_endgrent(void)
 enum nss_status _nss_example_setgrent(int stayopen)
 {
     __attribute__((cleanup(pthread_mutex_unlock_assertp))) pthread_mutex_t *_l = NULL;
-
     _l = pthread_mutex_lock_assert(&getgrent_data.mutex);
 
-    int r;
+    size_t count = 0;
+    char **groups = list_groups(&count);
 
+    getgrent_data.ext_db_entities = groups;
+    getgrent_data.entity_count = count;
     getgrent_data.array_index = 0;
 
     return NSS_STATUS_SUCCESS;
 }
-
 
 enum nss_status _nss_example_getgrent_r(struct group *result, char *buffer, size_t buflen, int *errnop)
 {
@@ -199,16 +206,25 @@ enum nss_status _nss_example_getgrent_r(struct group *result, char *buffer, size
     assert(result);
     assert(errnop);
 
-    __attribute__((cleanup(pthread_mutex_unlock_assertp))) pthread_mutex_t *_l = NULL;
+    char *bufPos = init_group_struct(result, buffer, buflen, NULL);
 
+    __attribute__((cleanup(pthread_mutex_unlock_assertp))) pthread_mutex_t *_l = NULL;
     _l = pthread_mutex_lock_assert(&getgrent_data.mutex);
 
-    if (getgrent_data.array_index == countof(group_array)) {
+    if (!getgrent_data.ext_db_entities)
         return NSS_STATUS_NOTFOUND;
-    }
 
-    const struct group *group_info = &group_array[getgrent_data.array_index];
-    *errnop = copy_to_group(group_info, result, buffer, buflen);
+    if (!getgrent_data.ext_db_entities[getgrent_data.array_index])
+        return NSS_STATUS_NOTFOUND;
+
+    GroupEntry entry;
+    int ret = get_group_by_name(getgrent_data.ext_db_entities[getgrent_data.array_index], &entry);
+
+    if (ret < 0)
+        return NSS_STATUS_UNAVAIL;
+
+    copy_group_entry(&entry, result, bufPos, buflen);
+    free_group_entry(&entry);
     getgrent_data.array_index++;
 
     return NSS_STATUS_SUCCESS;
